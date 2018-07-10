3a36af323d5842d6e3cd931dce28a81278565015
Estelle Maudet <e.maudet@qwant.com>
2018-07-10 10:43:46 +0200

    Add additional test to show how to use multiple models

--- fasttext_test.go
+++ fasttext_test.go
@@ -16,4 +16,34 @@ func TestSimple(t *testing.T) {
 	}
 	// fmt.Println(Predict("sushi in palo alto"))
 	// fmt.Println(Predict("muy caliente"))
+
+	fmt.Println("***")
+	fmt.Println("Using pointers to fasttext models")
+
+	ft_lang := New()
+	ft_lang.LoadModel("/home/estelle/go/src/github.com/cozy/cozy-stack/pkg/index/lid.176.ftz")
+
+	ft_intent := New()
+	ft_intent.LoadModel("/home/estelle/Projects/IntentClassification/QueryClassification/model.bin")
+
+	fmt.Println("Predicting languages:")
+
+	tags, err = ft_lang.PredictK("我是法国人", 6)
+	if err != nil {
+		panic(err)
+	}
+	for _, t := range tags {
+		fmt.Println(t.Label, t.Prob)
+	}
+
+	fmt.Println("Predicting intent:")
+
+	tags, err = ft_intent.PredictK("meteo paris", 6)
+	if err != nil {
+		panic(err)
+	}
+	for _, t := range tags {
+		fmt.Println(t.Label, t.Prob)
+	}
+
 }
